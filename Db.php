abd47a7a18bb46ee55121ee321c26dd35b81f444
Carlos Saltos <csaltos@gmail.com>
2021-07-20 19:58:11 +0300

    Registering user ID when generating challenge code

---
+++ Db.php
@@ -0,0 +1,46 @@
+<?php
+
+class Db {
+
+  public function __construct(&$session)
+  {
+    $this->session = &$session;
+    if (!isset($this->session['user_ids_by_challenge_code']))
+    {
+      $this->session['user_ids_by_challenge_code'] = [];
+    }
+    if (!isset($this->session['access_tokens_by_user_id']))
+    {
+      $this->session['access_tokens_by_user_id'] = [];
+    }
+  }
+
+  public function recordUserIdForChallengeCode($challenge_code, $user_id)
+  {
+    $this->session['user_ids_by_challenge_code'][$challenge_code] = $user_id;
+  }
+
+  public function recordAccessTokenForUserId($user_id, $access_token)
+  {
+    $this->session['access_tokens_by_user_id'][$user_id] = $access_token;
+  }
+
+  public function getUserIdForChallengeCode($challenge_code)
+  {
+    if (!isset($this->session['user_ids_by_challenge_code'][$challenge_code]))
+    {
+      return null;
+    }
+    return $this->session['user_ids_by_challenge_code'][$challenge_code];
+  }
+
+  public function getAccessTokenForUserId($user_id)
+  {
+    if (!isset($this->session['access_tokens_by_user_id'][$user_id]))
+    {
+      return null;
+    }
+    return $this->session['access_tokens_by_user_id'][$user_id];
+  }
+
+}
---
+++ layout/views/demo-request-permission.php
@@ -0,0 +1,43 @@
+<div class="uk-section uk-section-primary">
+  <div class="uk-container uk-container-small">
+    <h1>Talenteca Recruiter app integration demo</h1>
+  </div>
+</div>
+<div class="uk-section uk-section-default">
+  <div class="uk-container uk-container-small">
+    <h3>3. Request permission</h3>
+    <p class="uk-text-small">
+      Now we just need to redirect your local user session to Talenteca permission page.
+    </p>
+    <p class="uk-text-small">
+      This is the link we are going to use:
+    </p>
+    <p>
+      <div class="uk-text-meta uk-text-break tk-text-code"><?= $recruiter_app_auth_url ?></div>
+    </p>
+    <?php if (!is_null($errorMessage)) { ?>
+    <div class="uk-margin uk-align-center uk-width-2-3@l uk-text-danger">
+      <?= $errorMessage ?>
+    </div>
+    <?php } ?>
+    <div class="uk-grid">
+      <form class="uk-width-2-3@l" action="/?action=request-permission" method="post">
+        <fieldset class="uk-fieldset">
+          <div class="uk-margin">
+            <a class="uk-button uk-button-primary uk-align-center" href="<?= $recruiter_app_auth_url ?>">Request permission</a>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+    <p class="uk-text-small">
+      <strong class="uk-text-danger">IMPORTANT:</strong> Do not pass the user ID or any other extra sensible data in the redirect parameters of this URL, keep all the sensible data linked to the challenge code generated in an internal database.
+    </p>
+  </div>
+</div>
+<div class="uk-section uk-section-muted">
+  <div class="uk-container uk-container-small">
+    <p>
+      Find more information at the <a href="https://dev.talenteca.com/api/doc/">Talenteca API Doc</a>
+    </p>
+  </div>
+</div>
